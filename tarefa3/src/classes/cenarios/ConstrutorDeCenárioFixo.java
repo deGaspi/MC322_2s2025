6c51596195d6ee094f412c08eae87304e8fc2389
LipeAma <contato.feamaral@gmail.com>
2025-09-15 21:23:39 -0300

    adição do atributo listaDeFases na classe ConstrutorDeCenárioFixo.
    antes, o método gerar() deveria ser estático, porém as intruções da tarefa é pedido que o método gerar() seja não estático. Adicionando o atributo, o método se torna verdadeiramente não estático

--- tarefa3/src/classes/cenarios/ConstrutorDeCenárioFixo.java
+++ tarefa3/src/classes/cenarios/ConstrutorDeCenárioFixo.java
@@ -8,18 +8,16 @@ import classes.monstro.FalsoPatriota;
 import classes.monstro.Imperialista;
 import classes.interfaces.Fase;
 
-// Basicamente, a mesma coisa que você fez, mas com a implementação diferente
-public class ConstrutorDeCenárioFixo implements GeradorDeFases{
+public class ConstrutorDeCenárioFixo implements GeradorDeFases {
+    private ArrayList<Fase> listaDeFases = new ArrayList<Fase>();
 
     public ConstrutorDeCenárioFixo() {
-    }   
+    }
 
     public ArrayList<Fase> gerar(int n) {
         if (n < 1) {
             throw new IllegalArgumentException("Número de fases deve ser positivo");
         }
-
-         ArrayList<Fase> listaDeFases = new ArrayList<Fase>();
 
         // Primeira fase: Batalha com falsos patriotas para entrar na caverna.
         FaseDeCombate primeiraFase = new FaseDeCombate(TipoCenario.ENTRADA);
@@ -27,10 +25,8 @@ public class ConstrutorDeCenárioFixo implements GeradorDeFases{
         primeiraFase.addMonstro(new FalsoPatriota("Falso Patriota 2", 8, 2, 8));
         listaDeFases.add(primeiraFase);
 
-       
-
         // Ultima fase: Batalha com imperialista
-        var imperialista = new Imperialista("Imperialista", 10*n, 3*n, 30*n);
+        var imperialista = new Imperialista("Imperialista", 10 * n, 3 * n, 30 * n);
         FaseDeCombate ultimaFase = new FaseDeCombate(TipoCenario.CHEFE);
         ultimaFase.addMonstro(imperialista);
         n--;
@@ -38,18 +34,18 @@ public class ConstrutorDeCenárioFixo implements GeradorDeFases{
         if (n == 0)
             return listaDeFases;
 
-        // Fases do meio: Batalhas com falsos patriotas e entreguistas para descer os andares
+        // Fases do meio: Batalhas com falsos patriotas e entreguistas para descer os
+        // andares
         while (n > 1) {
             FaseDeCombate fase = new FaseDeCombate(TipoCenario.CAVERNA);
-            fase.addMonstro(new FalsoPatriota("Falso Patriota 1", 8*n, 2*n, 8*n));
-            fase.addMonstro(new FalsoPatriota("Falso Patriota 2", 8*n, 2*n, 8*n));
-            fase.addMonstro(new Entreguista("Entreguista", 8*n, 2*n, 8*n, imperialista));
+            fase.addMonstro(new FalsoPatriota("Falso Patriota 1", 8 * n, 2 * n, 8 * n));
+            fase.addMonstro(new FalsoPatriota("Falso Patriota 2", 8 * n, 2 * n, 8 * n));
+            fase.addMonstro(new Entreguista("Entreguista", 8 * n, 2 * n, 8 * n, imperialista));
             listaDeFases.add(fase);
             n--;
         }
 
         listaDeFases.add(ultimaFase);
-        
 
         return listaDeFases;
     }
