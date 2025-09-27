f445bf05c4dcec32a79df767dd5ea0db9b453eef
LipeAma <contato.feamaral@gmail.com>
2025-09-27 12:20:43 -0300

    Multiplicadores de dano, vida e xp, baseados na dificuldade do jogo.

--- tarefa4/src/classes/cenarios/ConstrutorDeCenárioFixo.java
+++ tarefa4/src/classes/cenarios/ConstrutorDeCenárioFixo.java
@@ -25,14 +25,50 @@ public class ConstrutorDeCenárioFixo implements GeradorDeFases {
             throw new IllegalArgumentException("Número de fases deve ser positivo");
         }
 
+        final float multiplicadorHP, multiplicadorDano, multiplicadorXP;
+        switch (dif) {
+            case FACIL:
+                multiplicadorHP = 1f;
+                multiplicadorDano = 1f;
+                multiplicadorXP = 1f;
+                break;
+            case MEDIO:
+                multiplicadorHP = 1.5f;
+                multiplicadorDano = 1.5f;
+                multiplicadorXP = 1.5f;
+                break;
+            case DIFICIL:
+                multiplicadorHP = 2f;
+                multiplicadorDano = 2f;
+                multiplicadorXP = 2f;
+                break;
+            default:
+                throw new AssertionError("Dificuldade inesperada, valor recebido: " + dif);
+        }
+
         // Primeira fase: Batalha com falsos patriotas para entrar na caverna.
         FaseDeCombate primeiraFase = new FaseDeCombate(TipoCenario.ENTRADA);
-        primeiraFase.addMonstro(new FalsoPatriota("Falso Patriota 1", 8, 2, 8,  getRandArma(dif)));
-        primeiraFase.addMonstro(new FalsoPatriota("Falso Patriota 2", 8, 2, 8,  getRandArma(dif)));
+        primeiraFase.addMonstro(new FalsoPatriota(
+                "Falso Patriota 1",
+                Math.round(8 * multiplicadorHP),
+                Math.round(2 * multiplicadorDano),
+                Math.round(8 * multiplicadorXP),
+                getRandArma(dif)));
+        primeiraFase.addMonstro(new FalsoPatriota(
+                "Falso Patriota 2",
+                Math.round(8 * multiplicadorHP),
+                Math.round(2 * multiplicadorDano),
+                Math.round(8 * multiplicadorXP),
+                getRandArma(dif)));
         listaDeFases.add(primeiraFase);
 
         // Ultima fase: Batalha com imperialista
-        var imperialista = new Imperialista("Imperialista", 10 * n, 3 * n, 30 * n,  getRandArma(dif));
+        var imperialista = new Imperialista(
+                "Imperialista",
+                Math.round(10 * multiplicadorHP),
+                Math.round(3 * multiplicadorDano),
+                Math.round(30 * multiplicadorXP),
+                getRandArma(dif));
         FaseDeCombate ultimaFase = new FaseDeCombate(TipoCenario.CHEFE);
         ultimaFase.addMonstro(imperialista);
         n--;
@@ -44,8 +80,18 @@ public class ConstrutorDeCenárioFixo implements GeradorDeFases {
         // Evento com entreguista pode ocorrer.
         while (n > 1) {
             FaseDeCombate fase = new FaseDeCombate(TipoCenario.CAVERNA);
-            fase.addMonstro(new FalsoPatriota("Falso Patriota 1", 8 * n, 2 * n, 8 * n,  getRandArma(dif)));
-            fase.addMonstro(new FalsoPatriota("Falso Patriota 2", 8 * n, 2 * n, 8 * n,  getRandArma(dif)));
+            fase.addMonstro(new FalsoPatriota(
+                "Falso Patriota 1",
+                Math.round(8 * multiplicadorHP),
+                Math.round(2 * multiplicadorDano),
+                Math.round(8 * multiplicadorXP),
+                getRandArma(dif)));
+            fase.addMonstro(new FalsoPatriota(
+                "Falso Patriota 2",
+                Math.round(8 * multiplicadorHP),
+                Math.round(2 * multiplicadorDano),
+                Math.round(8 * multiplicadorXP),
+                getRandArma(dif)));
             fase.adicionarEvento(new EventoEntregar(fase, imperialista));
             listaDeFases.add(fase);
             n--;
@@ -59,20 +105,22 @@ public class ConstrutorDeCenárioFixo implements GeradorDeFases {
     private static Arma getRandArma(Dificuldade dificuldade) {
         enum escolhas {
             CHINELO(1), LANÇA(1), REPIQUE(2), DESARMADO(2);
+
             public int peso;
+
             escolhas(int peso) {
                 this.peso = peso;
             }
         }
 
         int totalDosPesos = 0;
-        for (var candidato: escolhas.values()) {
+        for (var candidato : escolhas.values()) {
             totalDosPesos += candidato.peso;
         }
 
         int n = random.nextInt(totalDosPesos);
         escolhas escolhido = null;
-        for (escolhas candidato: escolhas.values()) {
+        for (escolhas candidato : escolhas.values()) {
             totalDosPesos -= candidato.peso;
             if (totalDosPesos <= n) {
                 escolhido = candidato;
