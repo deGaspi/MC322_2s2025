0b0cb85141c75d64877583950aa61dfb07d55f3d
Rafael Henrique de Gaspi <degaspirafael@gmail.com>
2025-09-28 19:36:41 -0300

    renomeação: postTurnInteract -> postKillInteract

--- tarefa4/src/classes/armas/Chinelo.java
+++ tarefa4/src/classes/armas/Chinelo.java
@@ -4,6 +4,6 @@ import classes.cenarios.Dificuldade;
 
 public class Chinelo extends Arma{
     public Chinelo(Dificuldade dificuldade){
-        super(3, 0, "Chinelo", dificuldade);
+        super(2, 0, "Chinelo", dificuldade);
     }
 }
